726e62f37437e73606e1e0fb95be2b1ce6f4cad3
Tung Truong <49893620+tbtruong@users.noreply.github.com>
2023-01-31 12:55:09 -0500

    changing frontend to have minimum files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <div>
+      <header>
         <text> Goodbye </text>
       </header>
     </div>
