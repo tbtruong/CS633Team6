9761c5d98879c5ceb3601f2cf219ab490d9a9bd7
Tung Truong <49893620+tbtruong@users.noreply.github.com>
2023-02-12 12:11:32 -0500

    added all components on page

--- frontend/src/Components/mainPage.tsx
+++ frontend/src/Components/mainPage.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import Typography from '@mui/material/Typography';
-import {Box, Button, TextField} from "@mui/material"
+import {Box, Button, FormControl, InputLabel, MenuItem, Select, SelectChangeEvent, TextField} from "@mui/material"
+import Slider from '@mui/material/Slider';
+
 
 const stylingH3 = {
     color: "black",
@@ -43,7 +45,42 @@ const buttonHolder = {
 
 }
 
-const mainPage = () => {
+const locationSelect = {
+    width: '100px'
+}
+
+const MainPage = () => {
+
+    const [age, setAge] = React.useState('');
+
+    let exampleJson = {
+        firstName: 'John',
+        middleName: 'Andrew',
+        lastName: 'Smith',
+        buId: 'U12345678'
+    }
+
+    const handleChange = (event: SelectChangeEvent) => {
+        setAge(event.target.value as string);
+    };
+
+    function valuetext(value: number) {
+        return `${value}°C`;
+    }
+
+    const marks = [
+        {
+            value: 0,
+            label: 'No Experience',
+        },
+        {
+            value: 100,
+            label: 'Very Experienced',
+        },
+    ];
+
+
+
     return (
         <Box sx={bigBox}>
             <Box sx={buttonHolder}>
@@ -88,9 +125,169 @@ const mainPage = () => {
             <Button variant="contained" size="large" sx={buttonHolder} >
               NEXT
             </Button>
+
+
+            <Typography>
+                BU ID: {exampleJson.buId} <br/>
+                First Name: {exampleJson.firstName}  <br/>
+                Middle Name: {exampleJson.middleName}  <br/>
+                Last Name: {exampleJson.lastName} <br/>
+            </Typography>
+
+            <Typography>
+                Organization you are currently working for
+            </Typography>
+            <TextField fullWidth required id="outlined-basic" label="Organization you are currently working for"  InputLabelProps={{ shrink: true }} variant="outlined" />
+
+            <Typography>
+                Please describe your main work responsibilties
+            </Typography>
+            <TextField fullWidth required id="outlined-basic"   InputLabelProps={{ shrink: true }} variant="outlined" />
+
+            <Typography>
+                Where are you located?
+            </Typography>
+            <Box>
+            <FormControl size="medium" sx={locationSelect}>
+                <InputLabel id="demo-simple-select-label" > Country </InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Country"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl size="medium" sx={locationSelect}>
+                <InputLabel id="demo-simple-select-label" > State </InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Country"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+
+            <FormControl size="medium" sx={locationSelect}>
+                <InputLabel id="demo-simple-select-label" >Timezone</InputLabel>
+                <Select
+                    labelId="demo-simple-select-label"
+                    id="demo-simple-select"
+                    value={age}
+                    label="Timezone"
+                    onChange={handleChange}
+                >
+                    <MenuItem value={10}>Ten</MenuItem>
+                    <MenuItem value={20}>Twenty</MenuItem>
+                    <MenuItem value={30}>Thirty</MenuItem>
+                </Select>
+            </FormControl>
+            </Box>
+
+            <Typography>
+                Please rank the following course project roles based on your experience
+            </Typography>
+            <Box>
+                <Box sx={{width: 300, display: 'flex', flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Typography>
+                    No Experience
+                </Typography>
+
+                <Typography>
+                    Very Experienced
+                </Typography>
+                </Box>
+
+            <Box sx={{ width: 300, display: 'flex', flexDirection: 'row' }}>
+                <Typography>
+                    Coding:
+                </Typography>
+                <Slider
+                    aria-label="Custom marks"
+                    defaultValue={20}
+                    getAriaValueText={valuetext}
+                    step={10}
+                    marks={marks}
+                />
+            </Box>
+
+            <Box sx={{ width: 300 }}>
+                <Typography>
+                    Requirements:
+                </Typography>
+                <Slider
+                    aria-label="Custom marks"
+                    defaultValue={20}
+                    getAriaValueText={valuetext}
+                    step={10}
+                    valueLabelDisplay="auto"
+                    marks={marks}
+                />
+            </Box>
+
+            <Box sx={{ width: 300 }}>
+                <Typography>
+                    Testing:
+                </Typography>
+                <Slider
+                    aria-label="Custom marks"
+                    defaultValue={20}
+                    getAriaValueText={valuetext}
+                    step={10}
+                    valueLabelDisplay="auto"
+                    marks={marks}
+                />
+            </Box>
+
+            <Box sx={{ width: 300 }}>
+                <Typography>
+                    UI Design:
+                </Typography>
+                <Slider
+                    aria-label="Custom marks"
+                    defaultValue={20}
+                    getAriaValueText={valuetext}
+                    step={10}
+                    valueLabelDisplay="auto"
+                    marks={marks}
+                />
+            </Box>
+
+            <Box sx={{ width: 300 }}>
+                <Typography>
+                    Project Management:
+                </Typography>
+                <Slider
+                    aria-label="Custom marks"
+                    defaultValue={20}
+                    getAriaValueText={valuetext}
+                    step={10}
+                    valueLabelDisplay="auto"
+                    marks={marks}
+                />
+            </Box>
+        </Box>
+
+        <Typography>
+           In a brief paragraph, please describe your experience in the above 5 categories.
+        </Typography>
+        <TextField sx={{width: '300px'}} fullWidth required id="outlined-basic"   InputLabelProps={{ shrink: true }} variant="outlined"  inputProps={{ maxLength: 12 }}/>
+
+
+
         </Box>
     );
 }
 
 
-export default mainPage;
+export default MainPage;
